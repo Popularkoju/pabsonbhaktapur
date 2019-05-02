481bf9b601518f77583892f07aa4f2e85234fd2c
Popularkoju <webpk144@gmail.com>
2019-05-02 11:33:48 +0545

    first commit

---
+++ app/src/main/java/lintend/pabson/bhaktapur/login_signupActivity/signupActivity.java
@@ -0,0 +1,16 @@
+package lintend.pabson.bhaktapur.login_signupActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lintend.pabson.bhaktapur.R;
+
+public class signupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.signup_layout);
+    }
+}
